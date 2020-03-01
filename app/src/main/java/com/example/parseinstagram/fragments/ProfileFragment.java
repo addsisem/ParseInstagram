720f6e44851c8cd557a7ce06a82c163d62d91994
*addsisem* <sisemorea@berea.edu>
2020-02-29 22:03:15 -0500

    Finished Part 2

---
+++ app/src/main/java/com/example/parseinstagram/fragments/ProfileFragment.java
@@ -0,0 +1,47 @@
+package com.example.parseinstagram.fragments;
+
+import android.util.Log;
+
+import com.example.parseinstagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostFragment {
+
+    @Override
+    protected void queryPosts() {
+
+        ParseQuery<Post> postQuery = ParseQuery.getQuery(Post.class);
+
+        postQuery.include(Post.KEY_USER);
+        postQuery.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        postQuery.setLimit(20);
+        postQuery.addDescendingOrder(Post.KEY_CREATED_KEY);
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+
+                if(e != null) {
+
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+
+                for(int i = 0; i < posts.size(); i++) {
+                    Post post = posts.get(i);
+                    Log.d(TAG, "Post " + post.getDescription() + " Username: " + post.getUser().getUsername());
+                }
+
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
